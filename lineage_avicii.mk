1c7d56f0004ffe9a7831cb3afdd83ae2124fe5c4
XxRagulxX <44401875+XxRagulxX@users.noreply.github.com>
2025-07-22 13:43:59 +0530

    avicii: Initialize for Evolution X 11.x

--- lineage_avicii.mk
+++ lineage_avicii.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from avicii device
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evoltion X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_avicii
